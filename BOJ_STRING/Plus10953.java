e5015ad00d6a26a298ec47729a3755eaf517d9ce
배정인 <38847677+jibae96@users.noreply.github.com>
2021-03-29 17:33:15 +0900

    백준 10953번 A+B-6

---
+++ BOJ_STRING/Plus10953.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Plus10953 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine(),",");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
