6115875fb28ded4bd07690b01712439f765746c8
배정인 <38847677+jibae96@users.noreply.github.com>
2021-03-29 17:31:02 +0900

    백준 11719번 그대로 출력하기2

---
+++ BOJ_STRING/Print11719.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.IOException;
+
+public class Print11719 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String temp = sc.nextLine();
+            System.out.println(temp);
+        }
+        sc.close();
+    }
+}
