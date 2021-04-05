75c7fe16d2ed009668433c61992e598b4a2dc651
배정인 <38847677+jibae96@users.noreply.github.com>
2021-04-05 19:45:39 +0900

    프로그래머스 level2 숫자의 표현

---
+++ Programmers/Level2/NumExpress.java
@@ -0,0 +1,22 @@
+public class NumExpress {
+    public int solution(int n) {
+        int answer = 0;
+        int sum;
+        for(int i=1; i<=n; i++){
+            sum=0;
+            for(int j=i; j<=n; j++){
+                sum += j;
+                if(sum==n){
+                    answer++;
+                    break;
+                }else if(sum>n) break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        NumExpress ne = new NumExpress();
+        ne.solution(15);
+    }
+}
