e77f234e3e225e2a0a10484426a3ae9714685a0b
배정인 <38847677+jibae96@users.noreply.github.com>
2021-05-26 21:45:02 +0900

    프로그래머스 level3 네크워크

---
+++ Programmers/Level3/Network.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Network {
+    public boolean[] visited;
+    int[] goX = {-1,0,1,0};
+    int[] goY = {0,1,0,-1};
+    int N;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        N = n;
+        
+        for(int i=0; i<n; i++){
+            if(visited[i]==false){
+                dfs(computers,i);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void dfs(int[][] computers, int n){
+        visited[n]=true;
+        for(int i=0; i<N; i++){
+            if(n!=i && computers[n][i]==1 && visited[i]==false)
+                dfs(computers,i);
+        }
+    }
+}
